ec1df389410490ef9825cd016bc948668754e55f
noronha <87375566+riken127@users.noreply.github.com>
2025-05-25 11:59:04 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,14 +1,11 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_PATH = os.path.join(BASE_DIR, "best.pt")
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-GAME_WIDTH = 1920
-GAME_HEIGHT = 1080
+MODEL_PATH = os.path.join(BASE_DIR, "runs", "detect", "train", "weights", "best.pt")
+SCREEN_WIDTH = 1366
+SCREEN_HEIGHT = 768
+GAME_WIDTH = 1366
+GAME_HEIGHT = 768
 OFFSET_TOP = 0
 OFFSET_LEFT = 0
 SENSITIVITY_SCALE = 2.6
-AIM_DELAY = 0.03
-CONFIDENCE = 0.5
-DETECTIONS_DIR = "detections"
